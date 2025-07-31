94360588103442e9cc66e29b46b1677ea64374f9
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-07-30 20:43:04 -0400

    Update set_hostname.pp

--- set_hostname.pp
+++ set_hostname.pp
@@ -11,17 +11,50 @@ case $host_ip {
 }
 
 if $facts['os']['family'] == 'windows' {
+  # Print current hostname
+  exec { 'Print-Windows-Hostname-Before':
+    command   => 'powershell.exe -Command "Write-Output \"Current Hostname: $(hostname)\""',
+    provider  => powershell,
+    logoutput => true,
+  }
+
+  # Rename Windows machine
   exec { "Rename-WindowsHostname":
     command   => "powershell.exe -Command \"Rename-Computer -NewName '${target_hostname}' -Force -Restart\"",
     provider  => powershell,
     logoutput => true,
     unless    => "powershell.exe -Command \"(hostname) -eq '${target_hostname}'\"",
+    require   => Exec['Print-Windows-Hostname-Before'],
   }
+
+  # This won’t run after restart, but it's here for completeness
+  exec { 'Print-Windows-Hostname-After':
+    command   => 'powershell.exe -Command "Write-Output \"New Hostname: $(hostname)\""',
+    provider  => powershell,
+    logoutput => true,
+    onlyif    => "powershell.exe -Command \"(hostname) -eq '${target_hostname}'\"",
+  }
+
 } else {
+  # Print current Linux hostname
+  exec { 'Print-Linux-Hostname-Before':
+    command   => '/bin/echo "Current Hostname: $(hostname)"',
+    logoutput => true,
+  }
+
+  # Set Linux hostname
   exec { "Set-Linux-Hostname":
     command   => "/usr/bin/hostnamectl set-hostname ${target_hostname}",
     path      => ['/usr/bin', '/bin'],
     logoutput => true,
-    unless    => "/usr/bin/hostname | grep ${target_hostname}",
+    unless    => "/bin/hostname | grep ${target_hostname}",
+    require   => Exec['Print-Linux-Hostname-Before'],
+  }
+
+  # Print new hostname
+  exec { 'Print-Linux-Hostname-After':
+    command   => '/bin/echo "New Hostname: $(hostname)"',
+    logoutput => true,
+    onlyif    => "/bin/hostname | grep ${target_hostname}",
   }
 }
