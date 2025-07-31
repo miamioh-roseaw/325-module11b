c60a6b368819465c44853afc02aa572f0883ee5b
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-07-30 20:23:20 -0400

    Update set_hostname.pp

--- set_hostname.pp
+++ set_hostname.pp
@@ -1,11 +1,8 @@
-# set_hostname.pp
-require 'yaml'
-
-$hostname_data = parseyaml(file('hostnames.yaml'))
+$hostname_data = loadyaml('hostnames.yaml')
 
 $hostname_data.each |$fqdn, $hostname| {
-  if $facts['os']['family'] == 'windows' {
-    if $fqdn == $facts['networking']['fqdn'] {
+  if $facts['networking']['fqdn'] == $fqdn {
+    if $facts['os']['family'] == 'windows' {
       notify { "Setting Windows hostname to ${hostname}": }
       exec { "Rename-WindowsHostname":
         command   => "powershell.exe -Command \"Rename-Computer -NewName '${hostname}' -Force -Restart\"",
@@ -13,9 +10,7 @@ $hostname_data.each |$fqdn, $hostname| {
         logoutput => true,
         unless    => "powershell.exe -Command \"(hostname) -eq '${hostname}'\"",
       }
-    }
-  } elsif $facts['os']['family'] == 'Debian' or $facts['os']['family'] == 'RedHat' {
-    if $fqdn == $facts['networking']['fqdn'] {
+    } elsif $facts['os']['family'] in ['Debian', 'RedHat'] {
       notify { "Setting Linux hostname to ${hostname}": }
       exec { "Set-Linux-Hostname":
         command   => "/usr/bin/hostnamectl set-hostname ${hostname}",
