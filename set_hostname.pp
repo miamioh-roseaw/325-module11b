7472ce87c7c42872434b58a2bed0c7686bdc050a
Anthony W Rose <83881859+miamioh-roseaw@users.noreply.github.com>
2025-07-30 20:54:17 -0400

    Update set_hostname.pp

--- set_hostname.pp
+++ set_hostname.pp
@@ -1,10 +1,22 @@
-class hostname_setter (
-  String $desired_hostname
-) {
+$hostname_map = inline_template("<%= YAML.load_file('/hostnames.yaml')['hosts'] %>")
 
-  exec { "set_hostname":
-    command => "/bin/hostnamectl set-hostname ${desired_hostname}",
-    path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-    unless  => "hostname | grep -q ${desired_hostname}",
+$local_ip = inline_template('<%= Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address %>')
+
+if $hostname_map[$local_ip] {
+  $target_hostname = $hostname_map[$local_ip]
+
+  notify { "Old hostname":
+    message => inline_template('<%= `hostname` %>'),
   }
+
+  exec { "Set hostname to ${target_hostname}":
+    command => "hostnamectl set-hostname ${target_hostname}",
+    path    => ['/bin', '/usr/bin'],
+  }
+
+  notify { "New hostname":
+    message => inline_template('<%= `hostname` %>'),
+  }
+} else {
+  fail("Unrecognized host IP: ${local_ip}")
 }
